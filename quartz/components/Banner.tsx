b28e69e6cd4c104debef68bb8007dcaf4ed8e0a0
maartentulp <maartentulp298@gmail.com>
2024-03-27 22:44:55 +0100

    Quartz sync: Mar 27, 2024, 10:44 PM

--- quartz/components/Banner.tsx
+++ quartz/components/Banner.tsx
@@ -18,7 +18,7 @@ function Banner({ fileData, displayClass }: QuartzComponentProps) {
   var imageUrl = imageUrls[randomIndex];
   */
 
-  Banner.afterDOM = `
+  Banner.beforeDom = `
   var imageUrl = "https://images.unsplash.com/photo-1557683316-973673baf926?q=80&w=2929&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   document.getElementById('bannerImage').src = imageUrl;
   }
