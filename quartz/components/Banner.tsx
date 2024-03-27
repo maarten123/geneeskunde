d2ef4e9ca7e630dc9b1dff9ed026023d4d701944
maartentulp <maartentulp298@gmail.com>
2024-03-27 22:18:31 +0100

    Quartz sync: Mar 27, 2024, 10:18 PM

--- quartz/components/Banner.tsx
+++ quartz/components/Banner.tsx
@@ -2,17 +2,24 @@ import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 
 function Banner({ fileData, displayClass }: QuartzComponentProps) {
-    const title = fileData.frontmatter?.title
-    if (title) {
-      return <h1 class={`article-title ${displayClass ?? ""}`}>{title}</h1>
-    } else {
-      return null
-    }
+      return <img src="https://images.unsplash.com/photo-1557683316-973673baf926?q=80&w=2929&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="100%" class={`banner ${displayClass ?? ""}`}></img>
   }
+
+
   Banner.css = `
-  .article-title {
+  .banner {
     margin: 2rem 0 0 0;
   }
   `
 
 export default (() => Banner) satisfies QuartzComponentConstructor
+
+/*
+function Banner({ fileData, displayClass }: QuartzComponentProps) {
+    const banner = fileData.frontmatter?.banner
+    if (banner) {
+      return <h1 class={`article-title ${displayClass ?? ""}`}>{banner}</h1>
+    } else {
+      return null
+    }
+  } */
