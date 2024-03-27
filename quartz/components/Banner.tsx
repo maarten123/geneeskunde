95585213ac483ed471d0d84dd97a3cfa0d52a851
maartentulp <maartentulp298@gmail.com>
2024-03-27 22:38:40 +0100

    Quartz sync: Mar 27, 2024, 10:38 PM

--- quartz/components/Banner.tsx
+++ quartz/components/Banner.tsx
@@ -2,18 +2,32 @@ import { QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
 
 function Banner({ fileData, displayClass }: QuartzComponentProps) {
-      return <img src="https://images.unsplash.com/photo-1557683316-973673baf926?q=80&w=2929&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" width="100%" class={`banner ${displayClass ?? ""}`}></img>
+      return <img id="bannerImage" src="" width="100%" class={`banner ${displayClass ?? ""}`}></img>
   }
 
+  var imageUrls = [
+    "https://images.unsplash.com/photo-1557683316-973673baf926?q=80&w=2929&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1554034483-04fda0d3507b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1548504778-b14db6c34b04?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+];
+
+  Banner.afterDOM = `
+  var randomIndex = Math.floor(Math.random() * imageUrls.length);
+  var imageUrl = imageUrls[randomIndex];
+  document.getElementById('bannerImage').src = imageUrl;
+  }
+  `
 
   Banner.css = `
   .banner {
     margin: 2rem 0 0 0;
-    max-height: 80px;
+    height: 80px;
     z-index: 1;
     object-fit: cover;
   }
   `
+
+
 
 export default (() => Banner) satisfies QuartzComponentConstructor
 
